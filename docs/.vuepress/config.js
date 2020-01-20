97a480ee810c0c80d4eb599b60a5624f491a8da8
Robin <302740969@qq.com>
2020-01-21 01:36:35 +0800

    sidbar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,17 +1,32 @@
 module.exports = {
   title: "Robin的个人博客",
-  description: "Personal Blog",
+  description: "王猛,个人博客",
   port: 8888,
+  locales:{
+    '/':{
+      lang:'zh-CN',
+    }
+  },
   themeConfig: {
+    lastUpdated: '上次更新',
     nav: [
       { text: "主页", link: "/" },
       { text: "前端基础", link: "/pages/front-basic/" },
       { text: "指导", link: "/pages/guide/" }
-    ]
+    ],
+    sidebar: {
+      '/pages/front-basic/':[
+        '',
+        'first',
+        'second'
+      ],
+      '/pages/guide/':[
+        '',
+        'first',
+        'second',
+        'three'
+      ]
+    }
   },
-  head: [
-    ['link', 
-        { rel: 'icon', href: '/icon.png' }
-    ],  
-],
+  head: [["link", { rel: "icon", href: "/icon.png" }]]
 };
